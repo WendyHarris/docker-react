c36e8ba262928841a6c015f0c7dcdad570f633e6
Wendy Harris <Wendy.Harris@ibm.com>
2022-03-22 13:06:13 -0400

    hello world

--- src/App.js
+++ src/App.js
@@ -5,7 +5,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hello World 
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello World 
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
